aa3094df25ba5311a2314bdca185995264ca87ed
Mondo <1armandosx@gmail.com>
2023-06-07 00:07:50 -0400

    Inital commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// main.js
+
+const reviews = [
+  {
+      text: "Placeholder review 1...",
+      link: "http://google.com/review1"
+  },
+  {
+      text: "Placeholder review 2...",
+      link: "http://google.com/review2"
+  },
+  {
+      text: "Placeholder review 3...",
+      link: "http://google.com/review3"
+  }
+];
+
+const carousel = document.querySelector('.carousel');
+let currentSlide = 0;
+
